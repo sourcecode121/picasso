b525db301ffa078236d95a4b5467f92820b04bda
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-10-17 19:34:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/picasso/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.picasso;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView1;
+    private ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView1 = (ImageView) findViewById(R.id.image_1);
+        imageView2 = (ImageView) findViewById(R.id.image_2);
+
+        Picasso picasso = Picasso.with(this);
+        picasso.setIndicatorsEnabled(true);
+
+        picasso.load(R.drawable.yoda)
+                .resize(700, 700)
+                .centerCrop()
+                .into(imageView1);
+
+        picasso.load(R.drawable.vader)
+                .resize(700, 700)
+                .centerCrop()
+                .into(imageView2);
+    }
+}
